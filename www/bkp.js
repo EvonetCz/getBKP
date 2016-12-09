1b5f23425995a1bc1ebd5db5d04d6438735de6b7
EvonetCz <michal.lukas@evonet.cz>
2016-12-09 10:24:39 +0100

    Update bkp.js

--- www/bkp.js
+++ www/bkp.js
@@ -3,7 +3,11 @@ var exec = require('cordova/exec');
 var bkp = {
    getKey: function(fnSuccess, fnError, data, cert){
       exec(fnSuccess, fnError, "GetBKP", "getKey", [data, cert]);
-   }
+   },
+   
+   getPKP: function(fnSuccess, fnError, data, cert){
+      exec(fnSuccess, fnError, "GetPKP", "getPKP", [data, cert]);
+   }   
 };
 
 module.exports = bkp;
